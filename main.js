32db75c2f49683e801695b1220eb5a753e2af32b
marigongora <maribelgongora67@gmail.com>
2025-03-25 21:38:50 -0500

    editado

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const pokemonList = document.getElementById("pokemonList")
+const pokemonDetail = document.getElementById("pokemonDetail")
+const pokemonInfo = document.getElementById ("pokemonInfo")
+const btnBack= document.getElementById ("btnBack")
+
 async function getPokemonData (pokemonID) {
     try {
         let res = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokemonID}`)
@@ -13,16 +17,41 @@ function displayPokemon (pokemon){
     const pokemonCard = document.createElement ("div")
     pokemonCard.classList.add("pokemon-card")
     pokemonCard.innerHTML= `
-    <img src="${pokemon.sprites.front_default}">
+    <img src="${pokemon.sprites.front_default}" alt= "imagen del ${pokemon.name}">
     <h3> ${pokemon.name} </h3>
     <p> ID: ${pokemon.id}</p>
     `
+    pokemonCard.addEventListener("click",()=> showPokemonDetail (pokemon))
+        
     pokemonList.appendChild(pokemonCard)
+    return true
+}
+function showPokemonDetail(pokemon){
+    console.log(pokemon.types.length)
+    for (i=0; i< pokemon.types.length;i++)
+        console.log(pokemon.types[i].type.name)
+        types.push(pokemon.types[i].type.name)
+
+    pokemonList.style.display= "none"
+    pokemonDetail.style.display= "block"
+    pokemonInfo.innerHTML= `
+    <img src= "${pokemon.sprites.front_default}" alt="image view front ${pokemon.name}">
+    <img src= "${pokemon.sprites.back_default}" alt="image view back ${pokemon.name}">
+    <h3>${types}</h3>
+    `
 }
 async function  loadPokedex(){
-    for (let i=1;i<50; i ++){
-let pokemon = await getPokemonData(i)
-displayPokemon (pokemon)
+    for (let i=1;i<5; i ++){
+    let pokemon = await getPokemonData(i)
+    displayPokemon (pokemon)
     }
 }
+btnBack.addEventListener("click",()=>{
+     pokemonList.style.display= "grid"
+    pokemonDetail.style.display= "none"
+    pokemon.type.name= "type"
+})
+
+
 loadPokedex()
+
